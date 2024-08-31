114011f5824daf91492d7baf89fb24390688ff96
Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2024-08-31 12:41:15 +0300

    Update 02-numbers-tasks.js

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -207,8 +207,15 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if(n === 1 || n === 0) {
+  return false;
+  }
+  if(Math.sqrt(n) % 2 !== 0) {
+  return true;
+} else {
+    return false;
+}
 }
 
 /**
