1a9f985eb2899c8881ab4cd0a73152e4485fe3b9
Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2024-09-06 14:29:24 +0300

    Update 03-arrays-tasks.js

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -313,8 +313,9 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  const a = arr.filter((item) => typeof item === 'number' && item > 0);
+  return a.length;
 }
 
 /**
