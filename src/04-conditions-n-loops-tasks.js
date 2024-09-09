f0ee7f3587af8b29abec1028f448be00701f6d7f
Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2024-09-09 17:34:23 +0300

    Update 04-conditions-n-loops-tasks.js

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -131,12 +131,10 @@ function isTriangle(a, b, c) {
  *
  */
 function doRectanglesOverlap(rect1, rect2) {
-  if (rect1.left + rect1.width <= rect2.left ||
-      rect2.left + rect2.width <= rect1.left) {
+  if (rect1.left + rect1.width <= rect2.left || rect2.left + rect2.width <= rect1.left) {
     return false;
   }
-  if (rect1.top + rect1.height <= rect2.top ||
-      rect2.top + rect2.height <= rect1.top) {
+  if (rect1.top + rect1.height <= rect2.top || rect2.top + rect2.height <= rect1.top) {
     return false;
   }
   return true;
