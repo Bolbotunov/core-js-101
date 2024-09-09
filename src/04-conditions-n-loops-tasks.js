a1e8adbe072f272b9988b84bed2c35e24a492419
Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2024-09-09 18:46:01 +0300

    Update 04-conditions-n-loops-tasks.js

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -294,8 +294,14 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  const arr = String(num).split('');
+  const arrNum = arr.map((item) => Number(item));
+  let total = arrNum.reduce((acc, item) => acc + item, 0);
+  if (total > 9) {
+    total = getDigitalRoot(total);
+  }
+  return total;
 }
 
 
