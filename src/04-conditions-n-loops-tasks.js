5e5163d5a876c755507d373451caf332ea11413a
Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2024-09-09 18:10:12 +0300

    Update 04-conditions-n-loops-tasks.js

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -186,8 +186,9 @@ function isInsideCircle(circle, point) {
  */
 function findFirstSingleChar(str) {
   for (let i = 0; i < str.length; i += 1) {
-    if (str.indexOf(str[i]) === str.lastIndexOf(str[i]))
-    return str[i];
+    if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) {
+      return str[i];
+    }
   }
   return null;
 }
